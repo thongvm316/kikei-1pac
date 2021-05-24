eb7b1c5d3f97f1b581e05f80d0379790e9728d51
vvquang <vv.quang@1pac.vn>
2021-05-24 21:38:23 +0700

    build: create tooltip component

---
+++ src/components/KTooltip.vue
@@ -0,0 +1,32 @@
+<template>
+  <span :tooltip="title" :position="position"><slot /></span>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+const FORM_PROPS = {
+  position: ['top', 'left', 'right', 'bottom']
+}
+
+Object.freeze(FORM_PROPS)
+
+export default defineComponent({
+  name: 'KTooltip',
+
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+
+    position: {
+      type: String,
+      default: 'top',
+      validator: (position) => {
+        return FORM_PROPS.position.includes(position)
+      }
+    }
+  }
+})
+</script>
