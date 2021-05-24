cf90bf5c3e3d67d9fa64be9d1d2972396dbbce5e
nghiatran <tran.nghia@1pac.vn>
2021-05-24 15:52:02 +0700

    feat: table tmp

---
+++ src/components/KSortIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="xxx">
+    <caret-up-icon />
+    <caret-down-icon class="u-mt-2" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import CaretUpIcon from '@/assets/ico_caret-up.svg'
+import CaretDownIcon from '@/assets/ico_caret-down.svg'
+
+export default defineComponent({
+  name: 'KSortIcon',
+
+  components: {
+    CaretUpIcon,
+    CaretDownIcon
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.xxx {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- src/views/Project/index.vue
+++ src/views/Project/index.vue
@@ -1,13 +1,52 @@
 <template>
-    <p>Project Page</p>
-    <router-link to="/project/edit">Go to edit page</router-link>
+  <table class="table">
+    <thead>
+      <tr>
+        <th class="yyy">
+          <span class="u-mr-8">更新日</span>
 
+          <k-sort-icon />
+        </th>
+        <th>クライアント / プロジェクト名</th>
+        <th>受注確度</th>
+        <th>リリース日</th>
+        <th>受注金額</th>
+        <th>計上月</th>
+        <th>グループ</th>
+        <th>営業担当</th>
+      </tr>
+    </thead>
+  </table>
+
+  <p>Project Page</p>
+  <router-link to="/project/edit">
+    <k-button
+      variant="primary"
+      size="md"
+    >
+    Go to Edit page
+    </k-button>
+  </router-link>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, defineAsyncComponent } from 'vue'
+const KButton = defineAsyncComponent(() => import('@/components/KButton'))
+const KSortIcon = defineAsyncComponent(() => import('@/components/KSortIcon'))
 
 export default defineComponent({
   name: 'ProjectPage',
+
+  components: {
+    KButton,
+    KSortIcon
+  }
 })
 </script>
+
+<style lang="scss" scoped>
+.yyy {
+  display: flex;
+  align-items: center;
+}
+</style>
