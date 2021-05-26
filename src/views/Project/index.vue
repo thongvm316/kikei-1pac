a3cad52f7586843123a3167f463f98c348c2d7b5
moon-hai <hai.lu@tokoin.io>
2021-05-26 09:17:15 +0700

    project page

--- src/views/Project/index.vue
+++ src/views/Project/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="u-scroll-x">
+  <section class="project__filters">
+    <div class="project__filter k-button-group">
+      <k-button variant="outline-primary" size="sm" icon>
+        <line-down-icon />CSVファイルダウンロード
+      </k-button>
+
+      <k-button variant="primary" size="sm" icon>
+        <line-add-icon />新規入出金追加
+      </k-button>
+    </div>
+
+    <k-pagination
+      class="project__filter"
+      v-model:value="page"
+      :total-item="71"
+      :per-page="10"
+      :click-handler="nextPage" />
+  </section>
+
+  <div class="k-table-responsive">
     <table class="k-table">
       <thead>
         <tr>
@@ -148,25 +167,38 @@
         </tr>
       </tbody>
     </table>
-  </div>
 
-  <p>Project Page</p>
-  <router-link to="/project/edit">
-    <k-button variant="primary" size="md"> Go to Edit page </k-button>
-  </router-link>
+    <project-empty-state />
+  </div>
 </template>
 
 <script>
 import { defineComponent, defineAsyncComponent } from 'vue'
+
 const KButton = defineAsyncComponent(() => import('@/components/KButton'))
 const KSortCaret = defineAsyncComponent(() => import('@/components/KSortCaret'))
+const KPagination = defineAsyncComponent(() => import('@/components/KPagination'))
+const LineDownIcon = defineAsyncComponent(() => import('@/assets/icons/ico_line-down.svg'))
+const LineAddIcon = defineAsyncComponent(() => import('@/assets/icons/ico_line-add.svg'))
+
+import ProjectEmptyState from './-components/ProjectEmptyState'
 
 export default defineComponent({
   name: 'ProjectPage',
 
+  data() {
+    return {
+      page: 1
+    }
+  },
+
   components: {
     KButton,
-    KSortCaret
+    KSortCaret,
+    KPagination,
+    LineDownIcon,
+    LineAddIcon,
+    ProjectEmptyState
   },
 
   methods: {
@@ -176,7 +208,30 @@ export default defineComponent({
 
     sortDesc() {
       console.log('Desc')
+    },
+
+    nextPage() {
+      console.log(this.page)
     }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+@import '@/styles/shared/mixins';
+
+.project__filters {
+  @include flexbox(flex-end, end);
+  flex-direction: column;
+  padding-bottom: 16px;
+  padding-top: 24px;
+}
+
+.project__filter {
+  align-self: flex-end;
+}
+
+.project__filter + .project__filter {
+  margin-top: 16px;
+}
+</style>
