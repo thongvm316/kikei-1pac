f430e64ea3d340f9c7c93ef19d4cd35b504d6bbf
nghiatran <tran.nghia@1pac.vn>
2021-05-31 10:31:42 +0700

    modify button

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -2,7 +2,33 @@
   <div>
     <a-date-picker />
     <a-button type="primary">Button</a-button>
-    <a-button type="primary" shape="circle" size="large" />
+    <a-button type="primary" shape="circle">
+      <template #icon>
+        <search-outlined />
+      </template>
+    </a-button>
+    <a-button type="primary" shape="circle" size="small">
+      <template #icon>
+        <search-outlined />
+      </template>
+    </a-button>
+
+    <a-button disabled type="primary" shape="circle" size="small">
+      <template #icon>
+        <search-outlined />
+      </template>
+    </a-button>
+
+    <a-button>Default</a-button>
+    <a-button type="danger" ghost>Danger</a-button>
+    <a-button disabled type="primary">
+      <template #icon><search-outlined /></template>
+      Search
+    </a-button>
+    <div>
+      <a-button disabled type="link">Link</a-button>
+      <a-button type="link">Link</a-button>
+    </div>
 
     <a-checkbox-group v-model:value="checkedList" :options="plainOptions" @change="onChange" />
 
@@ -17,9 +43,16 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
+import ProjectIcon from '@/assets/icons/ico_project.svg'
+import { SearchOutlined } from '@ant-design/icons-vue'
 
 export default defineComponent({
   name: 'Dashboard',
+
+  components: {
+    ProjectIcon,
+    SearchOutlined
+  },
 
   setup() {
     const plainOptions = ['Apple', 'Pear', 'Orange'];
