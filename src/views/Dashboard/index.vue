72bc4ab2018acd174fdd6e6b9bdd8ed47d45f8c1
nghiatran <tran.nghia@1pac.vn>
2021-05-31 21:41:18 +0700

    feat: table

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -48,10 +48,10 @@
       size="small" />
 
     <a-button type="primary" @click="showModal">Open Modal</a-button>
-    <a-modal v-model:visible="visible" width="800px" title="Basic Modal" @ok="handleOk">
+    <a-modal v-model:visible="visible" width="800px" title="Basic Modal" @ok="handleOk" >
       <template #footer>
         <a-button @click="handleCancel">クリア</a-button>
-        <a-button type="primary" @click="handleOk">
+        <a-button type="primary">
           <template #icon>
             <span class="btn-icon">
               <search-icon />
@@ -65,6 +65,31 @@
       <p>Some contents...</p>
       <p>Some contents...</p>
     </a-modal>
+
+    <div style="padding: 24px">
+      <a-table
+        :columns="columns"
+        :data-source="data"
+        :row-selection="rowSelection"
+        :pagination="false"
+        :expand-icon-column-index="expandIconColumnIndex"
+        :expand-icon-as-cell="false">
+        <template #action="{ record }">
+          <a-button :disabled="record.disabled" type="primary">確定</a-button>
+        </template>
+
+        <template #expandIcon="{ expanded, onExpand }">
+          <arrow-up-icon class="u-cursor-pointer" @click="onExpand" v-if="expanded" />
+          <arrow-down-icon class="u-cursor-pointer" v-else @click="onExpand" />
+        </template>
+
+        <template #expandedRowRender="{ record }">
+          <p style="margin: 0">
+            {{ record.description }}
+          </p>
+        </template>
+      </a-table>
+    </div>
   </div>
 </template>
 
@@ -72,13 +97,67 @@
 import { defineComponent, ref } from 'vue'
 import ProjectIcon from '@/assets/icons/ico_project.svg'
 import SearchIcon from '@/assets/icons/ico_search.svg'
+import ArrowDownIcon from '@/assets/icons/ico_arrow_down.svg'
+import ArrowUpIcon from '@/assets/icons/ico_arrow_up.svg'
+
+// test
+const columns = [
+  { title: 'Name', dataIndex: 'name', key: 'name' },
+  { title: 'Age', dataIndex: 'age', key: 'age' },
+  { title: 'Address', dataIndex: 'address', key: 'address' },
+  { title: 'Action', dataIndex: '', key: 'x', slots: { customRender: 'action',  } },
+  { title: '', dataIndex: 'xxx', key: 'xxx' }
+]
+
+const data = [
+  {
+    key: 1,
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+    disabled: true
+  },
+  {
+    key: 2,
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+    disabled: false
+  },
+  {
+    key: 3,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+    disabled: false
+  }
+]
+
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows)
+  },
+  onSelect: (record, selected, selectedRows) => {
+    console.log(record, selected, selectedRows)
+  },
+  onSelectAll: (selected, selectedRows, changeRows) => {
+    console.log(selected, selectedRows, changeRows)
+  },
+};
+
+const expandIconColumnIndex = 5
 
 export default defineComponent({
   name: 'Dashboard',
 
   components: {
     ProjectIcon,
-    SearchIcon
+    SearchIcon,
+    ArrowDownIcon,
+    ArrowUpIcon
   },
 
   setup() {
@@ -111,7 +190,11 @@ export default defineComponent({
       visible,
       showModal,
       handleOk,
-      handleCancel
+      handleCancel,
+      data,
+      columns,
+      rowSelection,
+      expandIconColumnIndex
     }
   }
 })
