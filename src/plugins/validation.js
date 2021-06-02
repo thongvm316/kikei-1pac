5601b773083d40bd13b0fd39099b91e618025c6e
Trung Nguyen <trung.nguyen@gumiviet.com>
2021-06-02 10:11:48 +0700

    fix(validate): fix validate

--- src/plugins/validation.js
+++ src/plugins/validation.js
@@ -1,6 +1,7 @@
 import { configure, defineRule, Form, Field, ErrorMessage } from "vee-validate"
 import { required, email, confirmed, between } from "@vee-validate/rules"
 import { localize } from "@vee-validate/i18n"
+import i18n from "@/locale"
 import en from "@vee-validate/i18n/dist/locale/en.json"
 import ja from "@vee-validate/i18n/dist/locale/ja.json"
 
@@ -18,32 +19,16 @@ const messages = {
   ja: { ...ja.messages, ...dictionary.ja }
 }
 
+const locale = i18n.global.locale
+
 // Define the rule globally
 defineRule("required", required)
 defineRule("email", email)
-defineRule("confirmed", confirmed)
-defineRule("between", between)
 
 configure({
-  validateOnBlur: true,
-  validateOnChange: true,
   validateOnInput: true,
-  validateOnModelUpdate: true
-})
-
-configure({
-  generateMessage: localize("en", {
-    messages: {
-      ...messages.en
-    }
-  })
-})
-
-configure({
-  generateMessage: localize("ja", {
-    messages: {
-      ...messages.ja
-    }
+  generateMessage: localize(`${locale}`, {
+    messages: { ...messages[locale] }
   })
 })
 
