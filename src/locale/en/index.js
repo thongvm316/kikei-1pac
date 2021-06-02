9b76589e462b40b3ebae7770fb30d45cc37c73b7
Trung Nguyen <trung.nguyen@gumiviet.com>
2021-06-02 15:04:43 +0700

    feat(table): config table

--- src/locale/en/index.js
+++ src/locale/en/index.js
@@ -1,9 +1,11 @@
 import breadcrumb from './breadcrumb'
 import sidebar from './breadcrumb'
 import company from './company'
+import table from './table'
 
 export default {
   breadcrumb,
   sidebar,
-  company
+  company,
+  table
 }
