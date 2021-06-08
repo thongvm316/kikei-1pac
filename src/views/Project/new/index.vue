41b971b2f36fbd6efcd12e745142ae96826a30fc
moon-hai <hai.lu@tokoin.io>
2021-06-08 23:31:10 +0700

    implement clone project

--- src/views/Project/new/index.vue
+++ src/views/Project/new/index.vue
@@ -1,15 +1,37 @@
 <template>
   <section class="project-new">
-    <project-form />
+    <project-form :project="project" />
   </section>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, onBeforeMount } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { getProject } from '../composables/useProject'
 import ProjectForm from '../-components/ProjectForm'
 
 export default defineComponent({
   name: 'ProjectNewPage',
+
+  setup() {
+    const route = useRoute()
+    const router = useRouter()
+    const project = {}
+
+    const fetchProject = async () => {
+      const projectId = route.query.selectedId
+      if (!projectId) return
+
+      project.value = await getProject(projectId)
+      router.replace({ query: {} })
+    }
+
+    onBeforeMount(() => { fetchProject() })
+
+    return {
+      project
+    }
+  },
 
   components: {
     ProjectForm
