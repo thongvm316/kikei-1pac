4097f39a0ab280f06880e86fcd6fd0f1dd0ad436
nghiatran <tran.nghia@vand.vn>
2021-07-19 14:51:48 +0700

    prevent show 2 error permission flash

--- src/store/flash/mutations.js
+++ src/store/flash/mutations.js
@@ -2,7 +2,7 @@ import { STORE_FLASH_MESSAGE, CLEAR_FLASH_MESSAGE } from '../mutation-types'
 
 const mutations = {
   [STORE_FLASH_MESSAGE](state, payload) {
-    if (state.message.message === payload.message) return
+    if (state.message.message === 'errors.account_0102') return // prevent show 2 error permission flashs.
 
     state.message = {
       ...state.message,
