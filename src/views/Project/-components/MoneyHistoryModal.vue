5e1f6c1ecadabaa1cdd597f8662dc3092f44a66d
nghiatran <tran.nghia@vand.vn>
2021-07-28 14:09:14 +0700

    money history modal

--- src/views/Project/-components/MoneyHistoryModal.vue
+++ src/views/Project/-components/MoneyHistoryModal.vue
@@ -1,16 +1,18 @@
 <template>
   <a-modal v-model:visible="visible" centered title="金額変更詳細" width="390px" class="money-history-modal">
     <template #footer>
-      <div v-for="item in moneyHistory" :key="item.id" class="money-history">
-        <p>{{ item.createdAt }}</p>
-        <p>{{ item.money }} ({{ item.adCurrency.code }})</p>
+      <div class="wrapper">
+        <div v-for="item in project.value?.adProjectMoneyHistories" :key="item.id" class="money-history">
+          <p>{{ item.createdAt }}</p>
+          <p>{{ item.money }} ({{ item.adCurrency.code }})</p>
+        </div>
       </div>
     </template>
   </a-modal>
 </template>
 
 <script>
-import { computed, defineComponent, ref } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'MoneyHistoryModal',
@@ -19,10 +21,8 @@ export default defineComponent({
     project: Object
   },
 
-  setup(props, { emit }) {
+  setup(_, { emit }) {
     const visible = ref()
-
-    const moneyHistory = computed(() => props.project.value?.adProjectMoneyHistory || [])
 
     const handleCancel = () => {
       emit('update:visible', false)
@@ -30,7 +30,6 @@ export default defineComponent({
 
     return {
       visible,
-      moneyHistory,
       handleCancel
     }
   }
@@ -43,9 +42,13 @@ export default defineComponent({
 .money-history-modal {
   .ant-modal-footer {
     text-align: left;
-    padding: 16px 24px 10px;
-    max-height: 172px;
+    padding: 16px 0 10px 24px;
+  }
+
+  .wrapper {
+    max-height: 153px;
     overflow-y: auto;
+    padding-right: 24px;
   }
 
   .money-history {
