32cfb3f5c7e8d44270b56a91ccfb1c333df365fd
truongthuan6631 <tazpaul6631@gmail.com>
2021-08-10 08:35:02 +0700

    fix(chart): fix 208, 209, 217, 218

--- src/views/FinancingChart/composables/useGetDetailChartService.js
+++ src/views/FinancingChart/composables/useGetDetailChartService.js
@@ -1,11 +1,11 @@
 import services from '@/services'
 const ChartService = services.get('ChartService')
 
-export default function useGetDetailChartService(data, params) {
+export default function useGetDetailChartService(data) {
   const getDetailChart = async () => {
     // eslint-disable-next-line no-useless-catch
     try {
-      const response = await ChartService.getDetailChart(data, params)
+      const response = await ChartService.getDetailChart(data)
       return response.data
     } catch (e) {
       throw e
